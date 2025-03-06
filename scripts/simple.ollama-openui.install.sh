d511fe72fc4c5fd74c162c1c7b72358e95d8a5b2
Adolfo Loustaunau <alousta@us.ibm.com>
2025-03-05 19:58:31 -0500

    Add files via upload

--- scripts/simple.ollama-openui.install.sh
+++ scripts/simple.ollama-openui.install.sh
@@ -9,16 +9,20 @@
 #podman exec -it open-webui bash
 #Then, try curl http://<YOUR_HOST_IP>:11434/v1/models (or curl http://ollama:11434/v1/models
 #ssh root@9.30.215.131
-#Passw0rd123!!!!!
-
-curl -fsSL https://ollama.com/install.sh | sh
+#psDDKC6?T3JJXgKYGwrjnpJV
+#Install ollama cli
+#curl -fsSL https://ollama.com/install.sh | sh
+#root | dSC7*Aaz8Py@qk!Cr?g@UZVT
 curl http://localhost:11434/v1/models
 podman ps -a
 podman stop open-webui
 podman rm open-webui
 #this podman run command in line 20 is to run Ollama locally in mac laptop
 #podman run -d --network slirp4netns:allow_host_loopback=true -p 3000:8080 -e OLLAMA_BASE_URL=http://host.containers.internal:11434 -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
-podman run -d --network host -p 3000:8080 -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+#podman run -d --network host -p 3000:8080 -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+podman run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://127.0.0.1:11434 --name open-webui --restart always ghcr.io/open-webui/open-webui:main
 podman ps -a
 curl http://localhost:11434/v1/models
-
+#once open-webui podman pod is up and running make sure to
+# confirm that open-web ui is up and change open-webui general settings===>connectivity ollama
+#connections...change it to 127.0.0.1:11434
